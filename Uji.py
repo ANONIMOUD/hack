53e12b561138a3433cc5d2e8fccd5496d41e2811
anonimoud <awengnobot@gmail.com>
2021-03-03 12:02:06 +0700

    Upload di Github

---
+++ Uji.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+#import system
+import os
+
+os.system('clear')
+
+#logo
+print '==========='
+print '  bio      '
+print '==========='
+
+#variable
+one=raw_input('siapa nama mu :')
+
+#tampilan
+print '==========='
+print '  bio      '
+print '==========='
+print 'kamu:',one
